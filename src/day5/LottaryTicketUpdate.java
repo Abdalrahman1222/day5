f40dad6dc525a5cf93a08550de387c70bc80f553
Abdalrahman1222 <abdalrahmanabdulla029@gmail.com>
2026-08-29 11:49:03 +0300

    Initial commit

---
+++ src/day5/LottaryTicketUpdate.java
@@ -0,0 +1,48 @@
+package day5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LottaryTicketUpdate {
+    private static final int LENGTH = 6;
+    private static final int MAX_LOTTARY_NUMBER = 69;
+    public static void main(String[] args) {
+        int[] ticket=generateTicketNumbers();
+        Arrays.sort(ticket);
+        int found=Arrays.binarySearch(ticket,9);
+        System.out.println(found);
+        printTicketNumbers(ticket);
+    }
+    public static int[] generateTicketNumbers(){
+        int [] ticket = new int[LENGTH];
+        Random random = new Random();
+        for(int i=0;i<ticket.length;i++){
+            int randomNumber;
+            do {
+                randomNumber=random.nextInt(MAX_LOTTARY_NUMBER)+1;
+            }while (search(ticket,randomNumber));
+
+            ticket[i]=randomNumber;
+        }
+        return ticket;
+    }
+    public static void printTicketNumbers(int [] ticket){
+//        for (int i=0;i<ticket.length;i++){
+//            System.out.println(ticket[i]);
+//        }
+        for(int number:ticket){
+            System.out.print(number+" | ");
+        }
+    }
+
+
+    public static boolean search(int [] array , int numberToSearchFor){
+        for (int value:array){
+            if (value==numberToSearchFor){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
